328ee2e926d74f3ca4482dcefc361d4350d6a6e7
Dragos Mihai <mihaidragosradu@gmail.com>
2020-07-02 18:48:32 +0300

    Updates

---
+++ precache-manifest.d2214403805b14a113282e9f659bcb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98640fa12311b76b3ea6c644620eb16d",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "72dcf9947f8650d97638",
+    "url": "/burger/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98fab10ebea13552480b",
+    "url": "/burger/static/js/2.2c94e264.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger/static/js/2.2c94e264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72dcf9947f8650d97638",
+    "url": "/burger/static/js/main.a697bb30.chunk.js"
+  },
+  {
+    "revision": "b70cca1f39cb2c1dd795",
+    "url": "/burger/static/js/runtime-main.95ddc443.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger.git/precache-manifest.5dac6140b81041f63411dc088e0a9661.js"
+  "/burger/precache-manifest.d2214403805b14a113282e9f659bcb12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
