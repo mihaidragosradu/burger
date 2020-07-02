596743bd285cb29e22cefea56490229a7f5aae49
Dragos Mihai <mihaidragosradu@gmail.com>
2020-07-02 13:11:00 +0300

    Updates

---
+++ precache-manifest.0921261333fcbc001a997ec44dc221c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fe838ff8de8188c8f69a5c05d1fedb",
+    "url": "/mihaidragosradu/burger.git/index.html"
+  },
+  {
+    "revision": "feaa76e246c9d9a18f81",
+    "url": "/mihaidragosradu/burger.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66ab2b783b2aaeb973d7",
+    "url": "/mihaidragosradu/burger.git/static/js/2.133d4b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mihaidragosradu/burger.git/static/js/2.133d4b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaa76e246c9d9a18f81",
+    "url": "/mihaidragosradu/burger.git/static/js/main.d24cdd6a.chunk.js"
+  },
+  {
+    "revision": "884711370634c3d3615c",
+    "url": "/mihaidragosradu/burger.git/static/js/runtime-main.a18b47c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mihaidragosradu/burger.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
